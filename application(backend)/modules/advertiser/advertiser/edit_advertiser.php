3753f30e12ab6a5ef716126e6412784e666d6378
Neil Cueto <neilcueto101@gmail.com>
2017-03-30 16:46:26 +0800

    added modals on edit advertiser modules/advertiser/advertiser/edit_advertiser.php

--- application(backend)/modules/advertiser/advertiser/edit_advertiser.php
+++ application(backend)/modules/advertiser/advertiser/edit_advertiser.php
@@ -12,11 +12,13 @@ if(isset($_GET['advertiser_id']))
     require 'form_data_advertiser.php';
 
 }
-
+	$show_modals = FALSE;
 if(xsrf_guard())
 {
     init_var($_POST['btn_cancel']);
     init_var($_POST['btn_submit']);
+	init_var($_POST['btn_submit2']);
+	init_var($_POST['btn_cancel2']);
     require 'components/query_string_standard.php';
     require 'subclasses/advertiser.php';
     $dbh_advertiser = new advertiser;
@@ -50,17 +52,33 @@ if(xsrf_guard())
         }
 
         if($message=="")
-        {
-
-            $dbh_advertiser->edit($arr_form_data);
+        {		
+			$show_modal = TRUE;
+        }	
+    }
+	if($_POST['btn_submit2'])
+	{
+		//$arr_form_data['question'] = $_POST['question'];
+		//debug($arr_form_data);
+		    $dbh_advertiser->edit($arr_form_data);
 
             redirect("listview_advertiser.php?$query_string");
-        }
-    }
+	}
+	
+	if($_POST['btn_cancel2'])
+	{
+		$show_modal = FALSE;
+	}
 }
 require 'subclasses/advertiser_html.php';
 $html = new advertiser_html;
+$modal_message ="Are you sure you want to continue?";
 $html->draw_header('Edit %%', $message, $message_type);
+	if($show_modals)
+	{
+		$html->draw_container_div_start_modal($modal_message);
+	}
+
 $html->draw_listview_referrer_info($filter_field_used, $filter_used, $page_from, $filter_sort_asc, $filter_sort_desc);
 $html->draw_hidden('advertiser_id');
 
